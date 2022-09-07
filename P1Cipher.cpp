18918079c837ccf0f071518bd73d49a151c7905b
cmmira <chrismg617@gmail.com>
2022-09-06 21:34:05 -0400

    Rotation 13 Function Commit

--- P1Cipher.cpp
+++ P1Cipher.cpp
@@ -3,9 +3,12 @@
 #include <string>
 using namespace std;
 
+void rot13(istream&, ostream&);
+
 int main(int argc, char *argv[])
 {
     istream *in = &cin;
+    ifstream file;
 
     if(argc == 1)
     {
@@ -14,7 +17,57 @@ int main(int argc, char *argv[])
     }
 
     string cmd(argv[1]);
-    cout << cmd <<endl;
-    
+    // Rotation 13 Command "-r"
+    if(cmd == "-r")
+    {
+        if(argc > 3)
+        {
+            cout << "TOO MANY ARGUMENTS" <<endl;
+            return 0;
+        }
+        if(argc == 3)
+        {
+            file.open(argv[2]);
+            if(!file.is_open())
+            {
+                cout << argv[2] << " FILE COULD NOT BE OPENED" <<endl;
+                return 0;
+            }
+            in = &file;
+        }
+
+        rot13(*in, cout);
+        return 0;
+    }
+    else
+    {
+        cout << cmd << "NOT A VALID COMMAND" <<endl;
+        return 0;
+    }
+
     return 0;
+}
+
+void rot13(istream& in, ostream& out)
+{
+    //const char alpha= "abcdefghijklmnopqrstuvwxyz"
+    const char *lower = "nopqrstuvwxyzabcdefghijklm";
+    const char *upper = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+    int ch;
+    while(1)
+    {
+        ch = in.get();
+        if (in.eof()) return;
+
+        if(islower(ch))
+        {
+            ch = lower[ch - 'a'];
+        }
+        else if(isupper(ch))
+        {
+            ch = upper[ch - 'A'];
+        }
+        out.put(ch);
+    }
 }
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ C++ Program with Commands that will perform 4 functions:
 - Perform a caesar encryption given a dictionary
 - Perform a caesar decryption given a dictionary
 - Create a random caesar cipher dictionary.
+
+This C++ Program uses the Command Line Arguments to read in the commands and optional/required files into the main() function.
+
+## Rotation 13 Letter Substitution
+With the command "-r" the program will rotate each letter 13 positions in the alphabet.
+    - This Command also allows an optional second argument which if the name of the file to read from. If not provided, the program will just read from standard input.
+    - The output of this command wil be generated to the standard output.
