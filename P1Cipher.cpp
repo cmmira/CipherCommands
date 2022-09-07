10c60d449b4e8d9844df279d571a5aaf39ca7860
cmmira <chrismg617@gmail.com>
2022-09-07 02:59:52 -0400

    Command for Encrypt/Decrypt using Caesar Cipher

--- P1Cipher.cpp
+++ P1Cipher.cpp
@@ -2,13 +2,18 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <map>
 using namespace std;
 
 void rot13(istream&, ostream&);
 void GenRanDict(ostream&);
+bool LDict(istream&, map<char,char>&, map<char,char>&);
+void caesar(map<char,char>&, istream&, ostream&);
 
 int main(int argc, char *argv[])
 {
+    map<char,char> cipher;
+    map<char,char> reverse;
     istream *in = &cin;
     ifstream file;
 
@@ -50,6 +55,50 @@ int main(int argc, char *argv[])
             return 0;
         }
         GenRanDict(cout);
+        return 0;
+    }
+    // Encrypt/Decrypt Input with Caesar Cipher Dictionary "-e"/"-d"
+    else if(cmd == "-e" || cmd == "-d")
+    {
+        if(argc > 4)
+        {
+            cout << "TOO MANY ARGUMENTS " <<endl;
+            return 0;
+        }
+        if(argc < 3)
+        {
+            cout << "DICTIONARY NOT PROVIDED " <<endl;
+            return 0;
+        }
+
+        ifstream dictionary(argv[2]);
+        if(!dictionary.is_open())
+        {
+            cout << argv[2] << " DICTIONARY COULD NOT BE OPENED" <<endl;
+            return 0;
+        }
+
+        if(!LDict(dictionary,cipher,reverse)) return 0;
+
+        if(argc == 4)
+        {
+            file.open(argv[3]);
+            if(!file.is_open())
+            {
+                cout << argv[3] << "FILE COULD NOT BE OPENED" <<endl;
+                return 0;
+            }
+            in = &file;
+        }
+
+        if(cmd == "-e")
+        {
+            caesar(cipher, *in, cout);
+        }
+        else
+        {
+            caesar(reverse, *in, cout);
+        }
         return 0;
     }
     else
@@ -104,5 +153,61 @@ void GenRanDict(ostream& out)
         nlet--;
         i++;
     }
+    return;
+}
+
+bool LDict(istream& in, map<char,char>& cipher, map<char,char>& reverse)
+{
+    string pair;
+    for (char ch='a'; ch <= 'z'; ch++)
+    {
+        in >> pair;
+        if(in.fail()) return false;
+
+        if(pair.length() != 2)
+        {
+            cout << "FORMATTING ERROR " << pair <<endl;
+            return false;
+        }
+        if(pair[0] != ch)
+        {
+            cout << "MISSING LETTER " << ch <<endl;
+            return false;
+        }
+        if(pair[0] == pair[1])
+        {
+            cout << "MAPPING ERROR " << ch <<endl;
+            return false;
+        }
+
+        cipher[pair[0]] = pair[1];
+
+        if(reverse.find(pair[1]) != reverse.end())
+        {
+            cout << "DUPLICATE CIPHERTEXT " << pair[1] <<endl;
+            return false;
+        }
+
+        reverse[pair[1]] = pair[0];
+    }
+    return true;
+}
+
+void caesar(map<char,char>& translate, istream& in, ostream& out)
+{
+    while(1)
+    {
+        char ch;
+        ch = in.get();
+        if(in.eof() || in.bad()) return;
+
+        if(islower(ch))
+            ch = translate[ch];
+        else if(isupper(ch))
+            ch = toupper(translate[tolower(ch)]);
+        
+        out.put(ch);
+    }
+    out << flush;
     return;
 }
--- README.md
+++ README.md
@@ -16,4 +16,19 @@ With the command __"-r"__ the program will rotate each letter 13 positions in th
 With the command __"-g"__ the program will generate a dictionary that will have each letter in the alphabet mapped to a different and unique ciphertext letter.
     - The generated cipher dictionary wil have pairs of letters with the first letter being the plaintext letter and the second letter being the corresponding ciphertext letter.
     - Each plaintext letter will only map to a unique ciphertext and no letter will map to itself.
+    - The generated dictionary will have the letter pairs shown by standard output.
 
+## Perform Caesar Encryption Given a Dictionary
+With the command __"-e"__ the program will perform a caesar cipher encryption using a provided dictionary file where each plaintext letter has a corresponding ciphertext letter paired with it. The encryption uses each letter as a plaintext letter and replaces it with the ciphertext letter that it is paired with in the dictionary.
+    - The format for the caesar cipher dictionary is a file with 26 pairs of letters, one per letter of the alphabet, in alphabetical order. Each pair has two letters: the plaintext letter and the ciphertext letter.
+    - This command also requires a second argument, which is the filename of the dictionary, and error will occur if there is no second argument provided.
+    - This command also supports an optional third argument which is the file to read from and encrypt the text in the second file. If it is not provided the program will read from standard input. 
+    - The output of this command will have the text input encrypted using the provided dictionary and shown through standard output. 
+
+
+## Perform Caesar Decryption Given a Dictionary
+with the command __"-d"__ the program will perfrom a caesar cipher decryption using the provided dictionary file where each plaintext letter has a corresponding ciphertext letter paired with it. The decryption uses each letter as a ciphertext letter and replaces it with the plaintext letter that it is mapped to in the dictionary.
+    - The format for the caesar cipher dictionary is a file with 26 pairs of letters, one per letter of the alphabet, in alphabetical order. Each pair has two letters: the plaintext letter and the ciphertext letter.
+    - This command also requires a second argument, which is the filename of the dictionary, and error will occur if there is no second argument provided.
+    - This command also supports an optional third argument which is the file to read from and decrypt the text in the second file. If it is not provided the program will read from standard input. 
+    - The output of this command will have the text input encrypted using the provided dictionary and shown through standard output.
